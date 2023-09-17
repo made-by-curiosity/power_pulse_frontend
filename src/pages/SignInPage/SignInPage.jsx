1ab74980e0bb6a325ec6d79cf9a49a291ed1c648
Karina-98 <115569415+Karina-98@users.noreply.github.com>
2023-09-17 20:02:29 +0200

    +

--- src/pages/SignInPage/SignInPage.jsx
+++ src/pages/SignInPage/SignInPage.jsx
@@ -1,7 +1,41 @@
 import { Container } from 'components/Container/Container';
+import { Formik, Form, Field } from 'formik';
+import { Button } from './SingInPage.styled';
 
 const SignInPage = () => {
-  return <Container>SignInPage</Container>;
+  const initialValues = {
+    email: '',
+    password: '',
+  };
+
+  const schema = "";
+
+  const onSubmit = (values, {resetForm}) => {
+    console.log(values)
+    resetForm();
+  }
+
+  return (
+    <Formik initialValues={initialValues} validationSchema={schema} onSubmit={onSubmit}>
+      <Form>
+        <Field
+          type="email"
+          name="email"
+          placeholder="Email"
+          label="Email"
+          autoComplete="off"
+          component={CustomInput } />
+        <Field type="password"
+          name="password"
+          placeholder="Password"
+          label="Password"
+          passwordBtn
+          autoComplete="off"
+          component={CustomInput} />
+        <Button type="submit">Submit</Button>
+      </Form>
+    </Formik>
+  );
 };
 
 export default SignInPage;
---
+++ src/pages/SignInPage/SingInPage.styled.js
@@ -0,0 +1,16 @@
+import  styled  from '@emotion/styled';
+
+export const Button = styled.button`
+  display: grid;
+  border-radius: 12px;
+  padding-left: 40px;
+  padding-right: 40px;
+  padding-top: 12px;
+  padding-bottom: 12px;
+  grid-gap: 10px;
+  background-color: #e6533c;
+  color: #EFEDE8;
+  line-height: 1.12 ;
+  size: 16px;
+
+`;
