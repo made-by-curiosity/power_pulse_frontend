44dd36e2b4c858def9c89538f72982ce1a232548
TurtlePower <agnkvdm@gmail.com>
2023-09-19 13:41:51 +0300

    update example

--- src/components/example/Example.jsx
+++ src/components/example/Example.jsx
@@ -1,9 +1,9 @@
 import { ExampleText, ExampleWrapper } from './Example.styled';
-import Button from '@mui/material/Button';
 import icons from '../../assets/icons/svg-sprite.svg';
 import { CustomInput } from 'components/CustomInput/CustomInput';
 import { Field, Form, Formik } from 'formik';
 import * as Yup from 'yup';
+import { MainButton } from 'components/MainButton/MainButton';
 
 const validationSchema = Yup.object({
   username: Yup.string()
@@ -16,6 +16,10 @@ const validationSchema = Yup.object({
     .max(20, 'Must be less  than 20 characters')
     .required('Password is required')
     .matches(/^[a-zA-Z0-9]+$/, 'Cannot contain special characters or spaces'),
+  age: Yup.number()
+    .positive()
+    .min(150, 'must me more than 150')
+    .required('Age is required'),
 });
 
 export const Example = () => {
@@ -23,11 +27,16 @@ export const Example = () => {
     username: '',
     password: '',
     color: '',
+    age: '',
   };
 
   const onSubmit = (values, { resetForm }) => {
     console.log(values);
     resetForm();
+  };
+
+  const handleClick = e => {
+    console.log(e.target);
   };
 
   return (
@@ -68,11 +77,27 @@ export const Example = () => {
             component={CustomInput}
             inputStyles={{ width: '200px' }}
           />
+          <Field
+            name="age"
+            label="Age"
+            type="text"
+            autoComplete="off"
+            component={CustomInput}
+            inputStyles={{ width: '200px' }}
+          />
+          <MainButton
+            type="submit"
+            text="Sign Up"
+            filled
+            onClick={handleClick}
+            btnStyles={{
+              width: 'max-content',
+              margin: '0 auto 10px',
+            }}
+          />
           <button type="submit">Submit</button>
         </Form>
       </Formik>
-
-      <Button variant="contained">Contained</Button>
       <ExampleText>Пример использования emotion стилей</ExampleText>
       <div>
         <svg width="36" height="36" fill="blue" stroke="yellow">
