3393870d6b183318e50d17a5c82d55328131dab0
Yurii <116266510+xtxthby@users.noreply.github.com>
2023-09-20 23:14:36 +0300

    add files4

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,7 +1,9 @@
 // import { Button } from '@mui/material';
 import { Container } from 'components/Container/Container';
 import { Blok, Pages, Title, Text, Foto, Img, Button } from './NotFoundPage.styled'
-import mo_img from '../../assets/images/mo-img/mo_main.jpg'
+// import mo_img from '../../assets/images/mo-img/mo_main.jpg'
+// import mo_img2 from '../../assets/images/mo-img/mo_main@2x.jpg'
+// import mo_img3 from '../../assets/images/mo-img/mo_main@3x.jpg'
 
 
 const NotFoundPage = () => {
@@ -27,7 +29,7 @@ const NotFoundPage = () => {
     </Blok>
 
     <Foto>
-        <Img src={mo_img} />
+        <Img/>
     </Foto>
     </Pages>
   </Container>;
--- src/pages/NotFoundPage/NotFoundPage.styled.jsx
+++ src/pages/NotFoundPage/NotFoundPage.styled.jsx
@@ -1,4 +1,14 @@
 import styled from '@emotion/styled';
+import mo_img from '../../assets/images/mo-img/mo_main.jpg';
+import mo_img2 from '../../assets/images/mo-img/mo_main@2x.jpg';
+import mo_img3 from '../../assets/images/mo-img/mo_main@3x.jpg';
+import ta_img from '../../assets/images/ta_img/ta_main.jpg';
+import ta_img2 from '../../assets/images/ta_img/ta_main@2x.jpg';
+import ta_img3 from '../../assets/images/ta_img/ta_main@3x.jpg';
+import bg_img from '../../assets/images/bg_img/bg_main.jpg';
+import bg_img2 from '../../assets/images/bg_img/bg_main@2x.jpg';
+import bg_img3 from '../../assets/images/bg_img/bg_main@3x.jpg';
+
 
 export const Pages = styled.div`
     position: relative;
@@ -118,7 +128,69 @@ export const Foto = styled.div`
     bottom: 0;
 `
 export const Img = styled.img`
-    display: block;
-    width: 100%;
-    height: auto;
+
+  background-image: url(${mo_img});
+  
+
+  @media (min-device-pixel-ratio: 2),
+    (-webkit-min-device-pixel-ratio: 2),
+    (min-resolution: 192dpi),
+    (min-resolution: 2dppx) {
+    & {
+      background-image: url(${mo_img2});
+    }
+  }
+  @media (min-device-pixel-ratio: 3),
+    (-webkit-min-device-pixel-ratio: 3),
+    (min-resolution: 192dpi),
+    (min-resolution: 3dppx) {
+    & {
+      background-image: url(${mo_img3});
+    }
+  }
+
+  @media(min-width: 768px) {
+    background-image: url(${ta_img});
+    @media (min-device-pixel-ratio: 2),
+      (-webkit-min-device-pixel-ratio: 2),
+      (min-resolution: 192dpi),
+      (min-resolution: 2dppx) {
+      & {
+        background-image: url(${ta_img2});
+      }
+    }
+    @media (min-device-pixel-ratio: 3),
+      (-webkit-min-device-pixel-ratio: 3),
+      (min-resolution: 192dpi),
+      (min-resolution: 3dppx) {
+      & {
+        background-image: url(${ta_img3});
+      }
+    }
+  }
+  @media(min-width: 1440px){
+   background-image: url(${bg_img});
+   @media (min-device-pixel-ratio: 2),
+      (-webkit-min-device-pixel-ratio: 2),
+      (min-resolution: 192dpi),
+      (min-resolution: 2dppx) {
+      & {
+        background-image: url(${bg_img2});
+      }
+    }
+    @media (min-device-pixel-ratio: 3),
+      (-webkit-min-device-pixel-ratio: 3),
+      (min-resolution: 192dpi),
+      (min-resolution: 3dppx) {
+      & {
+        background-image: url(${bg_img3});
+      }
+    }
+  }
+}
 `
+
+
+
+
+
