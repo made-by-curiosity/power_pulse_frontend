c99c921dc2ec403b04cdc7cd30e84c546c156845
Uliana <ulianaastra1@gmail.com>
2023-09-21 23:16:42 +0300

    update title

--- src/pages/WelcomePage/WelcomePage.styled.jsx
+++ src/pages/WelcomePage/WelcomePage.styled.jsx
@@ -2,8 +2,9 @@ import styled from '@emotion/styled';
 import bg_line from '../../assets/images/bg_img/bg_line.png';
 
 export const TitleText = styled.h2`
-width: 598px;
-height: 234px;
+width: 335px;
+height: 120px;
+
   font-size: 38px;
 font-style: normal;
 font-weight: 500;
@@ -19,6 +20,8 @@ background-repeat: no-repeat;
 background-position: left 0px bottom 607px;
 
   @media screen and (min-width: 768px) {
+    width: 598px;
+height: 234px;
 font-size: 70px;
   font-style: normal;
   font-weight: 500;
@@ -30,6 +33,7 @@ margin-bottom: 64px;
   }
 
  @media screen and (min-width: 1440px) {
+  
   background-position: left 80px bottom 449px;
 
   /* margin-left: 96px; */
