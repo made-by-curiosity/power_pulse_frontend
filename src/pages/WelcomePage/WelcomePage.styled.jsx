739ddc20285dc3a472c581635d9ab55ff7d3ca8c
Uliana <ulianaastra1@gmail.com>
2023-09-22 13:05:47 +0300

    add bg_line

--- src/pages/WelcomePage/WelcomePage.styled.jsx
+++ src/pages/WelcomePage/WelcomePage.styled.jsx
@@ -1,68 +1,18 @@
 import styled from '@emotion/styled';
 import bg_line from '../../assets/images/bg_img/bg_line.png';
 
-<<<<<<< Updated upstream
-export const TitleText = styled.h2`
-  width: 598px;
-  height: 234px;
-=======
-
 
 
 export const TitleText = styled.h2`
  width: 335px;
   height: 120px;
   margin-top: 65px;
->>>>>>> Stashed changes
   font-size: 38px;
   font-style: normal;
   font-weight: 500;
   line-height: 40px;
   letter-spacing: 0.38px;
   color: #efede8;
-<<<<<<< Updated upstream
-  margin-bottom: 52px;
- 
-  background-image: url(${bg_line});
-  background-repeat: no-repeat;
-  background-position: left 0px bottom 607px;
-
-  @media screen and (min-width: 768px) {
-    font-size: 70px;
-    font-style: normal;
-    font-weight: 500;
-    line-height: 78px;
-    letter-spacing: 0.7px;
-    margin-left: 32px;
-    margin-bottom: 64px;
-    background-position: left 16px bottom 684px;
-  }
-
-  @media screen and (min-width: 1440px) {
-    background-position: left 80px bottom 449px;
-    margin-bottom: 366px;
-  }
-`;
-
-export const WelcomeBtns = styled.div`
-  position: absolute;
-  gap: 14px;
-  bottom: 483px;
-  left: 20px;
-  display: flex;
-  gap: 14px;
-
-  @media screen and (min-width: 768px) {
-    bottom: 497px;
-    left: 32px;
-    gap: 20px;
-  }
-  @media screen and (min-width: 1440px) {
-    bottom: 246px;
-    left: 96px;
-    gap: 20px;
-  }
-=======
 
   background-image: url(${bg_line});
   background-repeat: no-repeat;
@@ -91,7 +41,6 @@ export const WelcomeBtns = styled.div`
   }
 `;
 
-
 export const WelcomeBtns = styled.div`
   display: flex;
   gap: 14px;
@@ -103,18 +52,12 @@ export const WelcomeBtns = styled.div`
      
   }
    
->>>>>>> Stashed changes
 `;
 
 export const WelcomeTitle = styled.div`
   position: relative;
-<<<<<<< Updated upstream
-`;
-=======
   /* background-image: url(${bg_line});
   background-repeat: no-repeat;
   background-position: left 0px bottom 607px; */
 `;
 
-
->>>>>>> Stashed changes
