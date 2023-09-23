c2ebb70c336eebb07464f42fe4d9a2880f14dbf2
TurtlePower <agnkvdm@gmail.com>
2023-09-23 23:13:37 +0300

    remove unused vars

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import { updateName, updateUserParams } from 'redux/auth/operations';
 
