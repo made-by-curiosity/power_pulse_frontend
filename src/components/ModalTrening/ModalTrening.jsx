2182b39901bd1b93d2767238e1167108bb186152
Karina-98 <115569415+Karina-98@users.noreply.github.com>
2023-09-24 01:18:06 +0200

    +

--- src/components/ModalTrening/ModalTrening.jsx
+++ src/components/ModalTrening/ModalTrening.jsx
@@ -1,98 +1,106 @@
-import { Backdrop } from '@mui/material';
 import {
   BurnedCaloriesDiv,
   BurnedCaloriesNumber,
   BurnedCaloriesText,
   Button,
   ButtonAdd,
+  DivColumn,
   DivTimer,
+  ImgDiv,
   ImgGif,
   ItemDiv,
   ItemTrening,
   ListTrening,
-  Modal,
   NameItem,
+  SpanButton,
   Svg,
   Text,
   ValueItem,
 } from './ModalTrening.styled';
 import icons from '../../assets/icons/svg-sprite.svg';
-
+import { CustomModal } from 'components/CustomModal/CustomModal';
+import { useState } from 'react';
 
 const example = {
   bodyPart: 'waist',
   equipment: 'body weight',
   gifUrl:
-    'https://res.cloudinary.com/ditdqzoio/image/upload/v1687127066/exercises/0001.gif',
-  name: '3/4 sit-up',
+    'https://res.cloudinary.com/ditdqzoio/image/upload/v1687127066/exercises/0002.gif',
+  name: '45° side bend',
   target: 'abs',
-  burnedCalories: 220,
+  burnedCalories: 323,
   time: 3,
 };
 
 export const ModalTrening = () => {
-  return (
-    <Backdrop>
-      <Modal>
-        <ImgGif src={example.gifUrl} alt="" />
-        <Text>Time</Text>
-        <DivTimer></DivTimer>
-        <Button type="button">
-          <Svg>
-            <svg fill="#EFEDE8">
-              <use href={icons + '#icon-playbutton'}></use>
-            </svg>
-          </Svg>
-        </Button>
+  const [isPlaying, setIsPlaying] = useState(false);
 
-        <BurnedCaloriesDiv>
-          <BurnedCaloriesText>Burned calories:</BurnedCaloriesText>
-          <BurnedCaloriesNumber>{example.burnedCalories}</BurnedCaloriesNumber>
-        </BurnedCaloriesDiv>
+  const togglePlaying = () => {
+    setIsPlaying(prevState => !prevState);
+  };
+
+  return (
+    <CustomModal
+      modalStyles={{
+        width: '335px',
+        height: '858px',
+        flexDirection: 'column',
+        alignItems: 'center',
+        display: 'flex',
+        padding: '0',
+      }}
+      modalTabletStyles={{ width: '694px', height: '550px',display: 'flex',
+    flexDirection: 'inherit',gap: '16px',
+    margin: '0px',
+    alignItems: 'flex-start', }}
+    >
+          <>
+              <DivColumn>
+        <ImgDiv>
+          <ImgGif src={example.gifUrl} alt="" />
+        </ImgDiv>
+          <Text>Time</Text>
+          <DivTimer></DivTimer>
+          <Button type="button" onClick={togglePlaying}>
+            <Svg>
+              <svg fill="#EFEDE8">
+                {isPlaying ? (
+                  <use href={icons + '#icon-pause'}></use>
+                ) : (
+                  <use href={icons + '#icon-playbutton'}></use>
+                )}
+              </svg>
+            </Svg>
+          </Button>
+
+          <BurnedCaloriesDiv>
+            <BurnedCaloriesText>Burned calories:</BurnedCaloriesText>
+            <BurnedCaloriesNumber>
+              {example.burnedCalories}
+            </BurnedCaloriesNumber>
+          </BurnedCaloriesDiv>
+              </DivColumn>
+              <DivColumn>
         <ListTrening>
-          {Object.entries(example).filter(([key, value]) => key !== 'gifUrl' && key !== 'burnedCalories').map(([key, value]) => (
-            <ItemTrening>
-              <ItemDiv>
-                <NameItem>{key}</NameItem>
-                <ValueItem>{value}</ValueItem>
-              </ItemDiv>
-            </ItemTrening>
-          ))}
-              
-          {/* <ItemTrening>
-            <ItemDiv>
-              <NameItem>Name</NameItem>
-              <ValueItem></ValueItem>
-            </ItemDiv>
-          </ItemTrening>
-          <ItemTrening>
-            <ItemDiv>
-              <NameItem>Target</NameItem>
-              <ValueItem></ValueItem>
-            </ItemDiv>
-          </ItemTrening>
-          <ItemTrening>
-            <ItemDiv>
-              <NameItem>Body Part</NameItem>
-              <ValueItem></ValueItem>
-            </ItemDiv>
-          </ItemTrening>
-          <ItemTrening>
-            <ItemDiv>
-              <NameItem>Equipment</NameItem>
-              <ValueItem></ValueItem>
-            </ItemDiv>
-          </ItemTrening>
-          <ItemTrening>
-            <ItemDiv>
-              <NameItem>Time</NameItem>
-              <ValueItem></ValueItem>
-            </ItemDiv>
-          </ItemTrening> */}
+          {Object.entries(example)
+            .filter(
+              ([key, value]) => key !== 'gifUrl' && key !== 'burnedCalories'
+            )
+            .map(([key, value]) => (
+              <ItemTrening>
+                <ItemDiv>
+                  <NameItem>{key}</NameItem>
+                  <ValueItem>{value}</ValueItem>
+                </ItemDiv>
+              </ItemTrening>
+            ))}
         </ListTrening>
 
-        <ButtonAdd>Add to diary</ButtonAdd>
-      </Modal>
-    </Backdrop>
+        <ButtonAdd>
+          <SpanButton>Add to diary</SpanButton>
+                  </ButtonAdd>
+                  </DivColumn>
+      </>
+    </CustomModal>
   );
 };
