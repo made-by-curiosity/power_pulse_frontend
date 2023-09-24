1009e8758848cf507a3c97b2541d1cb4d1ca4c8e
Nikita <evildeus666@gmail.com>
2023-09-24 16:19:29 +0300

    conflict fix

--- src/components/NewHeader/Header.jsx
+++ src/components/NewHeader/Header.jsx
@@ -7,13 +7,12 @@ import {
   HeaderContainer,
 } from './Header.styled';
 import { Logo } from './Logo/Logo';
-// import { BurgerMenu } from './BurgerMenu/BurgerMenu';
+import { BurgerMenu } from './BurgerMenu/BurgerMenu';
 import { useState } from 'react';
 import { NavBar } from './NavBar/NavBar';
 import { UserBar } from './UserBar/UserBar';
 
 export const Header = ({ setMenuIsOpen }) => {
-  // eslint-disable-next-line no-unused-vars
   const [IsLogedIn, setIsLogedIn] = useState(true);
 
   return (
