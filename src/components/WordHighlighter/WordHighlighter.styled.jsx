1fb11eb04d76b20e6eb82c0ab8d48af44128833e
TurtlePower <agnkvdm@gmail.com>
2023-09-24 21:50:15 +0300

    add responsive highlighter

--- src/components/WordHighlighter/WordHighlighter.styled.jsx
+++ src/components/WordHighlighter/WordHighlighter.styled.jsx
@@ -1,12 +1,21 @@
 import styled from '@emotion/styled';
 
 export const Highlighter = styled.svg`
+  width: 98px;
+  height: 35px;
+
   position: absolute;
   top: ${props => props.position.top + 6}px;
-  left: ${props => props.position.left - 15}px;
+  left: ${props => props.position.left - 6}px;
 
+  fill: #040404;
   stroke: #ef8964;
 
-  width: 185px;
-  height: 67px;
+  @media screen and (min-width: 768px) {
+    width: 185px;
+    height: 67px;
+
+    top: ${props => props.position.top + 6}px;
+    left: ${props => props.position.left - 15}px;
+  }
 `;
