94f28dc4fe52fbb762654f370ea58d84ba6e689f
TyomaShapoval <116449647+TyomaShapoval@users.noreply.github.com>
2023-09-24 22:58:54 +0300

    Visible Done

--- src/components/DayExercises/DayExercises.jsx
+++ src/components/DayExercises/DayExercises.jsx
@@ -7,28 +7,32 @@ import {
   WrapperA,
   NavP,
 } from './DayExercises.styled';
-import ico from '../../assets/icons/svg-sprite.svg'
+import ico from '../../assets/icons/svg-sprite.svg';
+import ExercisesTable from 'components/ExercisesTable/ExercisesTable';
 
-import { NavLink } from "react-router-dom";
-
-
+import { NavLink } from 'react-router-dom';
 
 export const DayExercises = () => {
+  const isVoid = true;
+
   return (
     <ProductWrapper>
       <TitleNav>
         <TitleP>Exercises</TitleP>
         <WrapperA>
-        <NavLink to="/exercises" style={{display:'flex', alignItems:'center'}}>
-          <NavP>Add exercise</NavP>
-          <SvgStyle >
-            <use href={ico + `#icon-nextarrow`}></use>
-          </SvgStyle>
-        </NavLink>
+          <NavLink
+            to="/exercises"
+            style={{ display: 'flex', alignItems: 'center' }}
+          >
+            <NavP>Add exercise</NavP>
+            <SvgStyle>
+              <use href={ico + `#icon-nextarrow1`}></use>
+            </SvgStyle>
+          </NavLink>
         </WrapperA>
-
       </TitleNav>
-      <NotProduct>Not found exercises</NotProduct>
+      {isVoid ? <ExercisesTable /> :
+      <NotProduct>Not found exercises</NotProduct>}
     </ProductWrapper>
   );
 };
--- src/components/ExercisesTable/ExercisesTable.styled.jsx
+++ src/components/ExercisesTable/ExercisesTable.styled.jsx
@@ -7,6 +7,13 @@ export const TitleColumn = styled.span`
   position: absolute;
   left: 0;
   top: -26px;
+  display: block;
+  width: 80px;
+  overflow: hidden;
+  height: 18px;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+
   @media (min-width: 768px) {
     display: none;
   }
@@ -40,30 +47,43 @@ export const HeadersTitle = styled.th`
   @media (min-width: 768px) {
     font-weight: 400;
     &:nth-of-type(1) {
-      margin-right: 186px;
+      white-space: nowrap;
+      width: 53px;
+      margin-right: 45px;
     }
     &:nth-of-type(2) {
-      margin-right: 87px;
+      margin-right: 82px;
     }
     &:nth-of-type(3) {
-      margin-right: 52px;
+      margin-right: 100px;
     }
     &:nth-of-type(4) {
-      margin-right: 59px;
+      margin-right: 56px;
+    }
+    &:nth-of-type(5) {
+      margin-right: 8px;
+      width: 78px;
+      overflow: hidden;
+      white-space: nowrap;
+      text-overflow: ellipsis;
     }
   }
   @media (min-width: 1440px) {
     &:nth-of-type(1) {
-      margin-right: 193px;
-    }
-    &:nth-of-type(2) {
-      margin-right: 124px;
-    }
-    &:nth-of-type(3) {
       margin-right: 70px;
     }
+    &:nth-of-type(2) {
+      margin-right: 107px;
+    }
+    &:nth-of-type(3) {
+      margin-right: 104px;
+    }
     &:nth-of-type(4) {
-      margin-right: 75px;
+      margin-right: 78px;
+    }
+    &:nth-of-type(5) {
+      margin-right: 20px;
+      overflow: revert;
     }
   }
 `;
@@ -87,16 +107,21 @@ export const CellTel = styled.td`
     margin-bottom: 42px;
   }
   &:nth-of-type(3) {
+    width: 297px;
+    height: 38px;
+    margin-bottom: 42px;
+  }
+  &:nth-of-type(4) {
     width: 81px;
     height: 38px;
     margin-right: 16px;
   }
-  &:nth-of-type(4) {
+  &:nth-of-type(5) {
     width: 80px;
     height: 38px;
     margin-right: 16px;
   }
-  &:nth-of-type(5) {
+  &:nth-of-type(6) {
     width: 76px;
     height: 38px;
     margin-right: 6px;
@@ -110,51 +135,61 @@ export const CellTel = styled.td`
     display: flex;
     align-items: center;
     &:nth-of-type(1) {
-      width: 204px;
+      width: 90px;
       margin-bottom: 0px;
       margin-right: 8px;
     }
     &:nth-of-type(2) {
-      width: 128px;
+      width: 132px;
       margin-bottom: 0px;
       margin-right: 8px;
     }
     &:nth-of-type(3) {
-      width: 90px;
+      width: 128px;
+      margin-bottom: 0px;
       margin-right: 8px;
     }
     &:nth-of-type(4) {
-      width: 90px;
+      width: 84px;
       margin-right: 8px;
     }
     &:nth-of-type(5) {
-      width: 80px;
+      width: 78px;
+      margin-right: 8px;
+    }
+    &:nth-of-type(6) {
+      width: 72px;
       margin-right: 12px;
     }
   }
 
   @media (min-width: 1440px) {
     &:nth-of-type(1) {
-      width: 212px;
+      width: 115px;
       margin-bottom: 0px;
       margin-right: 8px;
     }
     &:nth-of-type(2) {
-      width: 166px;
+      width: 157px;
       margin-bottom: 0px;
       margin-right: 8px;
     }
     &:nth-of-type(3) {
-      width: 105px;
+      width: 131px;
+      margin-bottom: 0px;
       margin-right: 8px;
     }
     &:nth-of-type(4) {
-      width: 105px;
+      width: 106px;
       margin-right: 8px;
     }
     &:nth-of-type(5) {
-      width: 110px;
-      margin-right: 12px;
+      width: 91px;
+      margin-right: 8px;
+    }
+    &:nth-of-type(6) {
+      width: 82px;
+      margin-right: 20px;
     }
   }
 `;
