662d09045077d0db2bb2c4ebc2fe2742818d89a3
TurtlePower <agnkvdm@gmail.com>
2023-09-26 08:25:39 +0300

    update user avatar in header

--- src/components/NewHeader/UserBar/UserButton/UserButton.jsx
+++ src/components/NewHeader/UserBar/UserButton/UserButton.jsx
@@ -1,11 +1,24 @@
+import { useSelector } from 'react-redux';
 import icons from '../../../../assets/icons/svg-sprite.svg';
-import { UserBtnSvg } from './UserButton.styled';
+import { AvatarContainer, UserBtnSvg } from './UserButton.styled';
+import { selectUser } from 'redux/auth/selectors';
 
 export const UserButton = () => {
+  const userInfo = useSelector(selectUser);
+  const { avatarUrl = null } = userInfo;
   return (
-    <UserBtnSvg>
-      <use href={icons + '#user'}></use>
-      <use href={icons + '#circle'}></use>
-    </UserBtnSvg>
+    <AvatarContainer>
+      {avatarUrl ? (
+        <img
+          src={avatarUrl}
+          alt="User's avatar"
+          style={{ width: '100%', height: '100%', borderRadius: '50%' }}
+        ></img>
+      ) : (
+        <UserBtnSvg>
+          <use href={icons + '#user'}></use>
+        </UserBtnSvg>
+      )}
+    </AvatarContainer>
   );
 };
