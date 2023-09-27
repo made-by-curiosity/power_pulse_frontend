46629fff76d3ac27ffe11874970914371307c8c3
TurtlePower <agnkvdm@gmail.com>
2023-09-27 21:28:10 +0300

    fix step clean on params page

--- src/components/ParamsForm/ParamsForm.jsx
+++ src/components/ParamsForm/ParamsForm.jsx
@@ -37,6 +37,7 @@ import { useDispatch } from 'react-redux';
 import { updateUserParams } from 'redux/auth/operations';
 
 import { useLocalStorage } from 'hooks/useLocalStorage';
+import { useEffect } from 'react';
 
 const today = new Date();
 const eighteenYearsAgo = new Date(
@@ -99,6 +100,12 @@ export const ParamsForm = () => {
   const tablet = useMediaQuery('(min-width:768px)');
 
   const [step, setStep] = useLocalStorage('step', 1);
+
+  useEffect(() => {
+    setStep(1);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const handleClickNext = () => {
     setStep(state => state + 1);
